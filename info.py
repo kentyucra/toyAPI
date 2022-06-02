74996d2c47b3e0f47950fdfe27f883b95036d9a0
Kent Yucra <mckkentmwk@gmail.com>
2022-06-02 14:13:23 +0100

    new endpoint lyrics

---
+++ info.py
@@ -0,0 +1,4 @@
+headers = {
+    'X-RapidAPI-Host': "shazam.p.rapidapi.com",
+    'X-RapidAPI-Key': "ee2aa630a7msh6b39cf23b9a2ac1p1439d7jsn1317531bed5c"
+    }
--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
+import http3
 
+client = http3.AsyncClient()
 app = FastAPI()
+
 
 @app.get("/")
 def index():
   return {
     "message": "Our first microservice"
+  }
+
+@app.get("/lyrics/{artist_name}/{song_name}")
+async def lirycs(artist_name: str, song_name: str):
+  url = f"http://api.lyrics.ovh/v1/{artist_name}/{song_name}"
+  print("url = ", url)
+  response = await client.get(url=url)
+  json_response = response.json()
+  return {
+    "artist": artist_name,
+    "song_name": song_name,
+    "lyrics": json_response["lyrics"]
   }
