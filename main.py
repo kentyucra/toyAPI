917e8cc78a94652ae2693f8a76eaab0761673c7b
Kent Yucra <mckkentmwk@gmail.com>
2022-06-02 15:05:36 +0100

    new endpoint top_songs

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import json
 from fastapi import FastAPI
 import requests
+from info import headers
+from datetime import datetime
 
 app = FastAPI()
 
@@ -13,9 +16,36 @@ def index():
 @app.get("/lyrics/{artist_name}/{song_name}")
 def lirycs(artist_name: str, song_name: str):
   url = f"http://api.lyrics.ovh/v1/{artist_name}/{song_name}"
-  print("url = ", url)
-  # response = await client.get(url=url)
-  # json_response = response.json()
   res = requests.get(url)
-  return res.json()
-  
+  json_res = res.json()
+  return {
+    "date": datetime.now(),
+    "artist_name": artist_name,
+    "song_name": song_name,
+    "lyrics": json_res["lyrics"]
+  }
+
+@app.get("/artist/top_songs/{artist_name}")
+def top_songs(artist_name: str):
+
+  url = "https://shazam.p.rapidapi.com/search"
+
+  querystring = {"term":"coldplay","offset":"0","limit":"10"}
+
+  response = requests.request("GET", url, headers=headers, params=querystring)
+  json_res = response.json()
+
+  avatar = json_res["artists"]["hits"][0]["artist"]["avatar"]
+  hits = json_res["tracks"]["hits"]
+  hits_names = []
+
+  for hit in hits:
+    hits_names.append({
+      "name": hit["track"]["title"]
+    })
+
+  return {
+    "artist_name": artist_name,
+    "avatar": avatar,
+    "hits": hits_names
+  }
